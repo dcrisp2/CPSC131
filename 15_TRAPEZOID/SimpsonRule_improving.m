32ba28394af67b82e8c19dead0aa07af00118737
Daniel Crisp <Daniel Crisp>
2016-11-25 17:05:16 -0500

    Started making more changes to SimpsonRule than I thought professor may like (harder to grade with abstraction). Sooo though it hasn't been tested yet, I kept the changes in this *improving.m file.

--- 15_TRAPEZOID/SimpsonRule_improving.m
+++ 15_TRAPEZOID/SimpsonRule_improving.m
@@ -20,7 +20,7 @@ function [vargout] = SimpsonRule( plotfun, method , X , fun , n )
     	fprintf('\nYet, %i is not. Breaking from function "SimpsonRule".',n);
     	fprintf('\n\nIf n=2, try using method=0 for Simpsons 1/3, or make n equal 3 or greater\n\n');
     	vargout(1:5) = 0;
-    	return;
+        return;
     end
     
 	% Determine step size using range of 'X' and number of steps 'n'
@@ -36,10 +36,10 @@ function [vargout] = SimpsonRule( plotfun, method , X , fun , n )
     else
         A=h*(3/8);
     end
-    
     estimated = A*(f_xa+f_xb); %Both first, and last points
 	for i=1:n-1
         xi = a + (h*i);
+        
         switch method
             case 0 % Simpson's 1/3... 1-4-2-4-2-4-1
                 if mod(i,2)==0
@@ -63,43 +63,36 @@ function [vargout] = SimpsonRule( plotfun, method , X , fun , n )
         end
 
         estimated = estimated + newval;
-	end
-    
-    % Plot function
-    if plotfun == 1
-        figure; ezplot(fun); title('Y = f(x)'),xlabel('X'); ylabel('Y');
     end
     
     % Use trapz(X,Y) as reference to 'real' estimate
     real_trapz = trapz(X,fun(X));
     error_trapz = ((real_trapz-estimated)/real_trapz)*100;
     
-    % Using simps.m from Garcia, Damien to get more accurate estimation of
-    % integral, at least more-so than from trapz.
-    % Even though it is an extension of matlab's trapz(). Code was found at...
-    % https://www.mathworks.com/matlabcentral/fileexchange/25754-simpson-s-rule-for-numerical-integration/content/simps.m
-    if exist('simps.m','file')
-        real_simps = simps(X,fun(X));
-        error_simps = ((real_simps-estimated)/real_simps)*100;
-    else
-        fprintf('\nsimps.m not found. real_simps and error_simps set to 0.\n');
-        real_simps = 0;
-        error_simps = 0;
-    end
+    % Using simps.m found on mathworks' file exchange as appears more accurate than trapz. See function for
+    % details.
+    [real_simps, error_simps] = call_simps(X,fun,estimated);
     
-    % Calculate error bounds using err function. Change what point you use
-    % to evaluate the error by using evalAt. Had defaulted to a+h..
-    %error_limit = errorlim(a,b,a+h,X,fun,n);
+    % Calculate error bounds using err function.
+    % Though, not currently using error_limit.
+    error_limit = errorlim(a,b,a+h,X,fun,n);
     
     % Aggregate values for 'raw', 'estimated', and 'error' into vargout.
     vargout=[estimated, real_trapz, error_trapz, real_simps, error_simps];
     
     % Display
-	display( vargout, method);
+	display( plotfun, method, fun, vargout );
 
 end
 
-function display( vargout , method )
+function display( plotfun, method, fun, vargout)
+    
+    % Plot function
+    if plotfun == 1
+        figure; ezplot(fun); title('Y = f(x)'),xlabel('X'); ylabel('Y');
+    end
+    
+    % Display text output
     if method==0
         fprintf('\nUsing Simpsons 1/3, the integral was estimated to be %f.',vargout(1));
     else
@@ -113,5 +106,21 @@ end
 function [error_limit] = errorlim(a,b,evalAt,X,fun,n)
     syms X;
     error_function = [-1/(180*n^4)]*[(b-a)^5]*diff(fun(X),X,4);
-    error_limit = vpa(subs(error_function,X,evalAt));
+    error_limit = vpa(subs(error_function,X,evalAt)); %Had defaulted to using a+h for evalAt
+end
+
+function [real_simps, error_simps] = call_simps(X,fun,estimated)
+    % Using simps.m from Garcia, Damien to get more accurate estimation of
+    % integral, at least more-so than from trapz.
+    % Even though it is an extension of matlab's trapz(). Code was found at...
+    % https://www.mathworks.com/matlabcentral/fileexchange/25754-simpson-s-rule-for-numerical-integration/content/simps.m
+    if exist('simps.m','file')
+        real_simps = simps(X,fun(X));
+        error_simps = ((real_simps-estimated)/real_simps)*100;
+    else
+        fprintf('\nsimps.m not found. real_simps and error_simps set to 0.\n');
+        real_simps = 0;
+        error_simps = 0;
+    end
+    
 end
