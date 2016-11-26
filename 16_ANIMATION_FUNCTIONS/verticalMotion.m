d2b319ba68776ab9401d1d0c0a2bf6f7e74ae733
dcrisp2 <crispd@mail.lcc.edu>
2016-11-26 14:52:34 -0500

    Attempted but could not complete HW ANIMATION. Spent good amount of time on verticalMotion, where the answers were derived from integrating force to velocity and position using initial conditions. symbolic expressions were used in order to do this, but plotting them didn't produce the correct plots.. a straight line was seen though it shouldn't have been.. the way matlab displays high value numbers in fractions instead of truncated doubles may have thrown me off. The billiards question went unanswered, and will now be left that way.

--- 16_ANIMATION_FUNCTIONS/verticalMotion.m
+++ 16_ANIMATION_FUNCTIONS/verticalMotion.m
@@ -11,30 +11,41 @@ function verticalMotion(x0,y0,z0,vx0,vy0,vz0,m)
     %Time
     tmax = 4; %seconds
     Nt = 500; %time steps
-    time = linspace(0,tmax,Nt);
-    syms t; %for easier integration
+    t = linspace(0,tmax,Nt);
+    syms time; %for easier integration
 
     %Force (F=ma)
     F = m*a;  %Only considering gravity so far...
     
     %Velocity
-    V = V0 + int(F,t);  %Velocity Vector
+    V = V0 + int(F,time);  %Velocity Vector
+    %V.y = V(2);
+    %Velocity = V.y(time);
     S = sqrt(sum(V.^2));  %Speed
     
     %Position
-    X = X0 + int(V,t);
+    X = X0 + int(V,time);
     
     %Energy
     E(1) = sqrt(sum((F.*X).^2));  %Potential Energy
     E(2) = 1/(2*m*S^2);  %Kinetic Energy
     TE = sum(E);  %Total Energy
-
-    %fprintf('\nVertical Position: %d meters',X(2));
-    %fprintf('\nVertical Velocity: %d meters/second',V(2));
-    %fprintf('\nPotential Energy: %d joules',E(1));
-    %fprintf('\nKinetic Energy: %d joules',E(1));
-    %fprintf('\nTotal Energy: %d joules',TE);
     
-    subplot
+	% Display plots of ball height, velocity, and energy
+    figure
+	subplot(2,2,1); plot(t,subs(X(2),t));
+	title('Height of Ball vs. Time'); xlabel('time (s)'); ylabel('Height (m)');
+	grid on
     
+	subplot(2,2,2); plot(t,subs(S,t));
+	title('Velocity vs. Time'); xlabel('time (s)'); ylabel('Velocity (m/s)');
+	grid on
+    
+    subplot(2,2,3); plot(t,subs(E(1),t),'b',t,subs(E(2),t),'r');
+	title('KE'); xlabel('time (s)'); ylabel('Kinetic Energy');
+	grid on
+    
+	subplot(2,2,4); plot(t,subs(TE,t));
+	title('PE'); xlabel('time (s)'); ylabel('Potential Energy');
+	grid on
 end
